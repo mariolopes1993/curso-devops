597fa3863006ab70342dd0149f1c6f015b331d8a
Mario Lopes <mariofilho@sempreceub.com>
2022-09-08 16:37:19 -0300

    teclado

---
+++ Python/04-ler_teclado.py
@@ -0,0 +1,9 @@
+digitar = input("digite algo: ")
+
+print("Tem espaços? ", digitar.isspace())
+print("É um numero? :", digitar.isnumeric())
+print("É um alfabético? :", digitar.isalpha())
+print("É um alfabético? :", digitar.isalnum())
+print("É um maiuscula? :", digitar.isupper())
+print("É um minuscula? :", digitar.islower())
+print("É um Capitalizada? :", digitar.istitle())
